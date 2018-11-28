626acd67cf199cce89664a832acbd54c1976dbd9
wupengforIT <wupengforIT@163.com>
2018-11-28 23:41:01 +0800

    分类基础页面完成

--- server/router.js
+++ server/router.js
@@ -2,19 +2,26 @@ var express = require('express');
 var router = express.Router();
 var DbUtils = require('./DbUtils');
 var fs = require('fs');
+var path = require('path');
 router.get('/getCommentsList', function (req, res) {
-    DbUtils.queryData('SELECT u.nikeName,c.comment FROM `comments` c,`user` u WHERE c.userid = u.id GROUP BY u.id,u.nikeName,c.`comment`', function (date) {
+    DbUtils.queryData('SELECT * from testName', function (date) {
         res.status(200).json(date);
     });
 });
 router.get('/', function (req, res) {
-    fs.readFile('C:/Users/ITSM/WebstormProjects/server/server/JDinServer/index.html', 'utf8', function (err, content) {
+    fs.readFile(path.join(__dirname, './JDinServer/index.html'), 'utf8', function (err, content) {
         if (err) {
             return res.status(500).end('服务器访问出错');
         }
         res.status(200).end(content);
     });
-
-
+});
+router.get('/toCategory',function (req,res) {
+    fs.readFile(path.join(__dirname, './JDinServer/category.html'), 'utf8', function (err, content) {
+        if (err) {
+            return res.status(500).end('服务器访问出错');
+        }
+        res.status(200).end(content);
+    });
 });
 module.exports = router;
